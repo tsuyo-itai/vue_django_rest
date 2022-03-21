8ef611fc8939cf80c4a5ea31fc2a7e63807b4f44
tsuyo-itai <suka_sudeilu@yahoo.co.jp>
2022-03-21 22:28:49 +0900

    vue axios環境構築

---
+++ src/vueapp/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import TestView from "../views/TestView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/test",
+    name: "test",
+    component: TestView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/vueapp/src/views/TestView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="(data, key) in Datas" :key="key">
+      <p>{{ data.name }}</p>
+      <p>{{ data.telnum }}</p>
+      <p>{{ data.comment }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Datas: [],
+    };
+  },
+  mounted() {
+    this.axios
+      .get("http://127.0.0.1:8000/api/v1/get_test_information/")
+      .then((response) => {
+        this.Datas = response.data;
+      });
+  },
+};
+</script>
